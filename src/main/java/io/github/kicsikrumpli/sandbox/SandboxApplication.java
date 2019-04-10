0488d8f830106d5e4e68ca213dc6093e8b82c227
Daniel David Kovacs <v-dakovacs@expedia.com>
2019-04-10 10:53:09 +0200

    why does it hang with virtualtime???

--- src/main/java/io/github/kicsikrumpli/sandbox/SandboxApplication.java
+++ src/main/java/io/github/kicsikrumpli/sandbox/SandboxApplication.java
@@ -2,8 +2,10 @@ package io.github.kicsikrumpli.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class SandboxApplication {
 
 	public static void main(String[] args) {
