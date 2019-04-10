3d108f0c5dde1dac087b201ea0726448504ed8fb
Daniel David Kovacs <v-dakovacs@expedia.com>
2019-04-10 13:58:55 +0200

    smore tests

--- src/test/java/io/github/kicsikrumpli/sandbox/FortuneControllerTest.java
+++ src/test/java/io/github/kicsikrumpli/sandbox/FortuneControllerTest.java
@@ -1,59 +1,43 @@
 package io.github.kicsikrumpli.sandbox;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-
-import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
-@Slf4j
-@ActiveProfiles("IT")
+@SpringBootTest
 public class FortuneControllerTest {
+    @MockBean
+    private FortuneStreamService mockFortuneService;
+
     @Autowired
-    WebTestClient client;
-    @Autowired
-    WireMockServer wiremockServer;
+    private FortuneController underTest;
 
     @Before
     public void setUp() {
-        var stubResponse = aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("{\"fortune\" : \"dummy fortune\"}");
-
-        wiremockServer.stubFor(get("/api/fortune").willReturn(stubResponse));
+        given(mockFortuneService.fetch(anyLong())).willReturn(Flux.just("one", "two", "three"));
     }
 
     @Test
     public void test() {
-        StepVerifier.create(client
-                        .get()
-                        .uri("/stream/fortune?num=3")
-                        .accept(TEXT_EVENT_STREAM)
-                        .exchange()
-                        .expectStatus().isOk()
-                        .returnResult(String.class)
-                        .getResponseBody()
-                        .log())
-                .expectNext("dummy fortune", "dummy fortune", "dummy fortune")
+        // GIVEN in setUp
+
+        // WHEN
+        Flux<String> fortuneStream = underTest.getFortuneStream(3);
+
+        // THEN
+        StepVerifier.create(fortuneStream)
+                .expectNext("one", "two", "three")
                 .verifyComplete();
     }
 }
---
+++ src/test/java/io/github/kicsikrumpli/sandbox/FortuneIT.java
@@ -0,0 +1,83 @@
+package io.github.kicsikrumpli.sandbox;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.test.StepVerifier;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
+@Slf4j
+@ActiveProfiles("IT")
+public class FortuneIT {
+    @Autowired
+    WebTestClient client;
+    @Autowired
+    WireMockServer wiremockServer;
+
+    @Before
+    public void setUp() {
+        var stubResponse = aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody("{\"fortune\" : \"dummy fortune\"}");
+
+        wiremockServer.stubFor(get("/api/fortune").willReturn(stubResponse));
+    }
+
+    @Test
+    public void testGetShouldRespondWithRequestedNumberOfFortuneCookies() {
+        StepVerifier.create(client
+                        .get()
+                        .uri("/stream/fortune?num=3")
+                        .accept(TEXT_EVENT_STREAM)
+                        .exchange()
+                        .expectStatus().isOk()
+                        .returnResult(String.class)
+                        .getResponseBody()
+                        .log())
+                .expectNext("dummy fortune", "dummy fortune", "dummy fortune")
+                .verifyComplete();
+    }
+
+    @Test
+    public void testGetShouldReturnBadRequestWhenNumberParameterIsInvalid() {
+        client.get()
+                .uri("/stream/fortune?num=NaN")
+                .accept(TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    public void testGetShouldReturnDefaultNumberOfFortuneCookiesWhenNotSpecified() {
+        StepVerifier.create(client
+                        .get()
+                        .uri("/stream/fortune")
+                        .accept(TEXT_EVENT_STREAM)
+                        .exchange()
+                        .expectStatus().isOk()
+                        .returnResult(String.class)
+                        .getResponseBody()
+                        .log())
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+}
